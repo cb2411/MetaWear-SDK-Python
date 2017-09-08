6738f19b2f7e8ec92e8d1bb59f18b53db25ac8fa
cb2411 <cb2411@ic.ac.uk>
2017-09-08 14:29:13 +0100

    added set_settings function to sensorfusion.py

--- pymetawear/modules/sensorfusion.py
+++ pymetawear/modules/sensorfusion.py
@@ -12,6 +12,7 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
 
+import re
 import time
 import logging
 from functools import wraps
@@ -19,7 +20,7 @@ from ctypes import cast, POINTER
 
 from pymetawear import libmetawear
 from pymetawear.exceptions import PyMetaWearException
-from pymetawear.mbientlab.metawear.cbindings import * 
+from pymetawear.mbientlab.metawear.cbindings import *
 from pymetawear.modules.base import PyMetaWearModule, Modules
 
 log = logging.getLogger(__name__)
@@ -32,6 +33,26 @@ def require_fusion_module(f):
     def wrapper(*args, **kwargs):
         if getattr(args[0], 'available', False) is False:
             raise PyMetaWearException("There is no Sensor Fusion "
+                                      "module on your MetaWear board!")
+        return f(*args, **kwargs)
+
+    return wrapper
+
+
+def require_bmi160(f):
+    def wrapper(*args, **kwargs):
+        if getattr(args[0], 'gyro_r_class', None) is None:
+            raise PyMetaWearException("There is not Gyroscope "
+                                      "module of your MetaWear board!")
+        return f(*args, **kwargs)
+
+    return wrapper
+
+
+def require_bmm150(f):
+    def wrapper(*args, **kwargs):
+        if getattr(args[0], 'mag_p_class', None) is None:
+            raise PyMetaWearException("There is not Magnetometer "
                                       "module on your MetaWear board!")
         return f(*args, **kwargs)
 
@@ -52,11 +73,41 @@ class SensorFusionModule(PyMetaWearModule):
         super(SensorFusionModule, self).__init__(board, debug)
         self.module_id = module_id
 
+        acc_sensors = [
+            Const.MODULE_ACC_TYPE_BMA255,  #3
+            Const.MODULE_ACC_TYPE_BMI160,  #1
+            Const.MODULE_ACC_TYPE_MMA8452Q #0
+        ]
+        acc_sensor_odr = [
+            AccBma255Odr,
+            AccBmi160Odr,
+            AccMma8452qOdr
+        ]
+        acc_sensor_fsr = [
+            AccMma8452qRange,
+            AccBoschRange,
+            AccBoschRange
+        ]
+
         if self.module_id == Modules.MBL_MW_MODULE_NA:
             # No sensor fusion present!
             self.available = False
+            # No gyroscope present! TODO: How to tell which is missing?
+            self.gyro_r_class = None
+            self.gyro_o_class = None
+            # No magnetometer present!
+            self.mag_o_class = None
+            self.mag_p_class = None
         else:
             self.available = True
+            self.gyro_r_class = GyroBmi160Range
+            self.gyro_o_class = GyroBmi160Odr
+            self.mag_o_class = MagBmm150Odr
+            self.mag_p_class = MagBmm150Preset
+            for count, a in enumerate(acc_sensors):
+                if module_id == a:
+                    self.acc_o_class = acc_sensor_odr[count]
+                    self.acc_r_class = acc_sensor_fsr[count]
 
         self.board = board
 
@@ -84,6 +135,60 @@ class SensorFusionModule(PyMetaWearModule):
 
         self._callbacks = {}
         self._waiting_for_processor = False
+
+        self.acc_odr = {}
+        self.gyr_odr = {}
+        self.mag_odr = {}
+        self.acc_fsr = {}
+        self.gyr_fsr = {}
+        self.mag_power_presets = {}
+
+        self.acc_current_odr = 0
+        self.acc_current_fsr = 0
+
+        acc_odr_class = None
+        acc_fsr_class = None
+
+        self.high_frequency_stream = False # TODO: Is this just for acc?
+
+        # FOR ACCELEROMETER
+        if self.acc_o_class is not None:
+            # Parse possible output data rates for this accelerometer.
+            for key, value in vars(self.acc_o_class).items():
+                if re.search('^_([0-9]+)\_*([0-9]*)Hz', key) and key is not None:
+                    self.acc_odr.update({key[1:-2].replace("_", "."):value})
+
+        if self.acc_r_class is not None:
+            # Parse possible output data ranges for this accelerometer.
+            for key, value in vars(self.acc_r_class).items():
+                if re.search('^_([0-9]+)G', key) and key is not None:
+                    self.acc_fsr.update({key[1:-1]:value})
+
+        # FOR GYROSCOPE
+        if self.gyro_o_class is not None:
+            # Parse possible output data rates for this gyroscope.
+            for key, value in vars(self.gyro_o_class).items():
+                if re.search('_([0-9]+)Hz', key) and key is not None:
+                    self.gyr_odr.update({key[1:-2]: value})
+
+        if self.gyro_r_class is not None:
+            # Parse possible ranges for this gyroscope.
+            for key, value in vars(self.gyro_r_class).items():
+                if re.search('_([0-9]+)dps', key) and key is not None:
+                    self.gyr_fsr.update({key[1:-3]: value})
+
+        # FOR MAGNETOMETER
+        if self.mag_p_class is not None:
+            # Parse possible presets for this magnetometer.
+            for key, value in vars(self.mag_p_class).items():
+                if re.search('^([A-Z\_]*)', key) and isinstance(value,int):
+                    self.mag_power_presets.update({key.lower():value})
+
+        if self.mag_o_class is not None:
+            # Parse possible data rates for this magnetometer.
+            for key, value in vars(self.mag_o_class).items():
+                if re.search('^_([0-9\_]*Hz)', key) and isinstance(value,int):
+                    self.mag_odr.update({key:value})
 
         if debug:
             log.setLevel(logging.DEBUG)
@@ -162,16 +267,128 @@ class SensorFusionModule(PyMetaWearModule):
                                                   mode)
         libmetawear.mbl_mw_sensor_fusion_write_config(self.board)
 
-    @require_fusion_module
-    def set_acc_range(self, acc_range):
-        libmetawear.mbl_mw_sensor_fusion_set_acc_range(self.board,
-                                                       acc_range)
-        libmetawear.mbl_mw_sensor_fusion_write_config(self.board)
+    def _get_odr(self, value, sensor):
+        if sensor == 'acc':
+            sorted_ord_keys = sorted(self.acc_odr.keys(), key=lambda x: (float(x)))
+        elif sensor == 'gyr':
+            sorted_ord_keys = sorted(self.gyr_odr.keys(), key=lambda x: (float(x)))
+        else:
+            print('Cannot set odr for this sensor')
+        diffs = [abs(value - float(k)) for k in sorted_ord_keys]
+        min_diffs = min(diffs)
+        if min_diffs > 0.5:
+            raise ValueError(
+                "Requested ODR ({0}) was not part of possible values: {1}".format(
+                    value, [float(x) for x in sorted_ord_keys]))
+        return float(value) if sensor == 'acc' else self.gyr_odr.get(sorted_ord_keys[diffs.index(min_diffs)])
+
+    def _get_fsr(self, value, sensor):
+        if sensor == 'acc':
+            sorted_ord_keys = sorted(self.acc_fsr.keys(), key=lambda x: (float(x)))
+        elif sensor == 'gyr':
+            sorted_ord_keys = sorted(self.gyr_fsr.keys(), key=lambda x: (float(x)))
+        else:
+            print('Cannot set fsr for this sensor')
+        diffs = [abs(value - float(k)) for k in sorted_ord_keys]
+        min_diffs = min(diffs)
+        if min_diffs > 0.1:
+            raise ValueError(
+                "Requested FSR ({0}) was not part of possible values: {1}".format(
+                    value, [x for x in sorted_ord_keys]))
+        return float(value) if sensor == 'acc' else self.gyr_fsr.get(sorted_ord_keys[diffs.index(min_diffs)])
+
+
+    def _get_power_preset(self, value):
+        if value.lower() in self.mag_power_presets:
+            return self.mag_power_presets.get(value.lower())
+        else:
+            raise ValueError("Requested power preset ({0}) was not part of "
+                             "possible values: {1}".format(value.lower(), [self.mag_power_presets.keys()]))
+
+    @require_bmi160
+    @require_bmm150
+    def set_settings(self, acc_range=None, acc_rate=None, gyr_range=None, gyr_rate=None, mag_power_preset=None):
+        """ Set accelerometer, gyroscope and magnetometer settings.
+
+        Can be called with multiple or only one setting:
+        .. code-block:: python
+            mwclient.sensorfusion2.set_settings(acc_rate=200.0, acc_range=8.0)
+
+        will give the same result as
+        .. code-block:: python
+            mwclient.sensorfusion.set_settings(acc_rate=200.0)
+            mwclient.sensorfusion.set_settings(acc_range=8.0)
+
+        albeit that the latter example makes two writes to the board.
+
+        Power preset for magnetometer should be a string:
+        .. code-block:: python
+            mwclient.sensorfusion.set_settings(power_preset="LOW_POWER")
+
+        Call :meth:`~get_possible_settings` to see which values
+        that can be set for the sensors.
+
+        :param float acc_rate:   The frequency of accelerometer updates in Hz.
+        :param float acc_range:  The measurement range in the unit ``g``.
+        :param float gyr_rate:   The frequency of gyroscope updates in Hz.
+        :param float gyr_range:  The measurement range in the unit ``dps``,
+                                 degrees per second.
+        :param str power_preset: The power preset, influencing the data rate,
+                                 accuracy and power consumption
+        """
+
+        if acc_rate is not None:
+            self.acc_current_odr = acc_rate
+            acc_odr = self._get_odr(acc_rate, 'acc')
+            if self._debug:
+                log.debug("Setting Accelerometer ODR to {0}".format(acc_odr))
+            libmetawear.mbl_mw_acc_set_odr(self.board, c_float(acc_odr))
+
+        if acc_range is not None:
+            self.acc_current_fsr = acc_range
+            acc_fsr = self._get_fsr(acc_range, 'acc')
+            if self._debug:
+                log.debug("Setting Accelerometer FSR to {0}".format(acc_fsr))
+            libmetawear.mbl_mw_acc_set_range(self.board, c_float(acc_fsr))
+
+        if gyr_rate is not None:
+            gyr_odr = self._get_odr(gyr_rate, 'gyr')
+            if self._debug:
+                log.debug("Setting Gyroscope ODR to {0}".format(gyr_odr))
+            libmetawear.mbl_mw_gyro_bmi160_set_odr(self.board, gyr_odr)
+
+        if gyr_range is not None:
+            gyr_fsr = self._get_fsr(gyr_range, 'gyr')
+            if self._debug:
+                log.debug("Setting Gyroscope FSR to {0}".format(gyr_fsr))
+            libmetawear.mbl_mw_gyro_bmi160_set_range(self.board, gyr_fsr)
+
+        if (acc_rate is not None) or (acc_range is not None):
+            self.acc_current_odr = acc_rate
+            self.acc_current_fsr = acc_range
+            libmetawear.mbl_mw_acc_write_acceleration_config(self.board)
+
+        if (gyr_rate is not None) or (gyr_range is not None):
+            libmetawear.mbl_mw_gyro_bmi160_write_config(self.board)
+
+        if mag_power_preset is not None:
+            pp = self._get_power_preset(mag_power_preset)
+            if self._debug:
+                log.debug("Setting Magnetometer power preset to {0}".format(pp))
+            libmetawear.mbl_mw_mag_bmm150_set_preset(self.board, pp)
+
+        # raise NotImplementedError()
 
     @require_fusion_module
     def set_gyro_range(self, gyro_range):
         libmetawear.mbl_mw_sensor_fusion_set_gyro_range(self.board,
                                                         gyro_range)
+        libmetawear.mbl_mw_sensor_fusion_write_config(self.board)
+
+    @require_fusion_module
+    def set_acc_range(self, acc_range):
+        libmetawear.mbl_mw_sensor_fusion_set_acc_range(self.board,
+                                                       acc_range)
         libmetawear.mbl_mw_sensor_fusion_write_config(self.board)
 
     @require_fusion_module
@@ -195,9 +412,6 @@ class SensorFusionModule(PyMetaWearModule):
         raise NotImplementedError()
 
     def get_possible_settings(self):
-        raise NotImplementedError()
-
-    def set_settings(self):
         raise NotImplementedError()
 
     @require_fusion_module
